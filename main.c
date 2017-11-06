50db94836e373b8012cae0d50110ec489135a726
François Lallemand <frlallemand@protonmail.com>
2017-11-06 19:09:04 +0100

    ajoute le choix de la règle

--- main.c
+++ main.c
@@ -9,105 +9,65 @@
 #include <float.h>
 #include <stdbool.h>
 
-// Fonction de test, retourne 1 si le test d'une fonction échoue
-// N'est pas une fonction de test unitaire au sens strict du termes
-// puisque certains tests consiste en un affichage dont il faut
-// vérifier la consistance
-int test(){
-    // Test de l'initialisation
-    printf("On initialise le plateau de jeu :\n");
-    Etat * etat = etat_initial();
-    afficheJeu(etat);
+int main(int argc, char *argv[]) {
+	Regle r = MAX;
+    if(argc == 2){
+		if (!strcmp("robuste", argv[1])){
+			r = ROBUSTE;
+		} else if(!strcmp("max", argv[1])) {
+			r = MAX;
+		} else {
+			printf("Règle non reconnue, veuillez choisir entre max et robuste.\n");
+			return 1;
+		}
+	} else {
+		printf("Pas de règle spécifiée, utilisation de la règle max.\n");
+	}
+	Coup * coup;
+	FinDePartie fin;
 
-    // Test d'un coup humain
-    afficheJeu(etat);
+	// initialisation
+	Etat * etat = etat_initial();
 
-    // Test d'un coup ordi
-    printf("On joue un coup en tant qu'ordi dans la troisième colonne:\n");
-	Coup * coup = nouveauCoup(0);
-	jouerCoup(etat, coup);
-	coup = nouveauCoup(5);
-	jouerCoup(etat, coup);
-	coup = nouveauCoup(0);
-	jouerCoup(etat, coup);
-	coup = nouveauCoup(4);
-	jouerCoup(etat, coup);
-	coup = nouveauCoup(1);
-	jouerCoup(etat, coup);
-	coup = nouveauCoup(6);
-	jouerCoup(etat, coup);
-	coup = nouveauCoup(1);
-	jouerCoup(etat, coup);
-	coup = nouveauCoup(5);
-	jouerCoup(etat, coup);
-	coup = nouveauCoup(0);
-	jouerCoup(etat, coup);
+	// Choisir qui commence :
+	printf("Qui commence (0 : humain, 1 : ordinateur) ? ");
+	scanf("%d", &(etat->joueur) );
 
+	// boucle de jeu
+	do {
+		printf("\n");
+		afficheJeu(etat);
+
+		if ( etat->joueur == 0 ) {
+			// tour de l'humain
+
+			do {
+				coup = demanderCoup();
+			} while ( !jouerCoup(etat, coup) );
+
+			free(coup);
+		}
+		else {
+			// tour de l'Ordinateur
+
+			ordijoue_mcts(etat, TEMPS, r);
+
+		}
+
+		fin = testFin( etat );
+	}	while ( fin == NON ) ;
+
+	printf("\n");
 	afficheJeu(etat);
 
-	FinDePartie fin = testFin( etat );
 	if ( fin == ORDI_GAGNE )
 		printf( "** L'ordinateur a gagné **\n");
 	else if ( fin == MATCHNUL )
 		printf(" Match nul !  \n");
-	else if ( fin == HUMAIN_GAGNE )
-		printf( "** BRAVO, l'ordinateur a perdu  **\n");
 	else
-		printf(" Partie non finie !  \n");
+		printf( "** BRAVO, l'ordinateur a perdu  **\n");
 
-    return 0;
-}
+	free(etat);
+	return 0;
 
-int main(int argc, char *argv[]) {
-    if(argc == 1){
-        Coup * coup;
-        FinDePartie fin;
-
-        // initialisation
-        Etat * etat = etat_initial();
-
-        // Choisir qui commence :
-        printf("Qui commence (0 : humain, 1 : ordinateur) ? ");
-        scanf("%d", &(etat->joueur) );
-
-        // boucle de jeu
-        do {
-            printf("\n");
-            afficheJeu(etat);
-
-            if ( etat->joueur == 0 ) {
-                // tour de l'humain
-
-                do {
-                    coup = demanderCoup();
-                } while ( !jouerCoup(etat, coup) );
-
-                free(coup);
-            }
-            else {
-                // tour de l'Ordinateur
-
-                ordijoue_mcts(etat, TEMPS, ROBUSTE);
-
-            }
-
-            fin = testFin( etat );
-        }	while ( fin == NON ) ;
-
-        printf("\n");
-        afficheJeu(etat);
-
-        if ( fin == ORDI_GAGNE )
-            printf( "** L'ordinateur a gagné **\n");
-        else if ( fin == MATCHNUL )
-            printf(" Match nul !  \n");
-        else
-            printf( "** BRAVO, l'ordinateur a perdu  **\n");
-
-        free(etat);
-        return 0;
-    }
-    else if (strcmp("test", argv[1]) == 0){
-        return test();
-    }
 }
